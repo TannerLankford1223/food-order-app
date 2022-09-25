0c88460d11e37a6d3207e29de639bba7784a5e0d
TannerLankford1223 <T.Lankford730@gmail.com>
2022-09-25 17:26:22 -0500

    Add checkout form and validation

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,103 @@
+import { useRef, useState } from "react";
+
+import styles from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const submitOrderHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalIsValid = isFiveChars(enteredPostal);
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+  };
+
+  const nameControlStyles = `${styles.control} ${
+    formInputValidity.name ? "" : styles.invalid
+  }`;
+
+  const streetControlStyles = `${styles.control} ${
+    formInputValidity.street ? "" : styles.invalid
+  }`;
+
+  const cityControlStyles = `${styles.control} ${
+    formInputValidity.city ? "" : styles.invalid
+  }`;
+
+  const postalControlStyles = `${styles.control} ${
+    formInputValidity.postalCode ? "" : styles.invalid
+  }`;
+
+  return (
+    <form className={styles.form} onSubmit={submitOrderHandler}>
+      <div
+        className={nameControlStyles}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputValidity.name && <p>Please enter a valid name</p>}
+      </div>
+      <div className={streetControlStyles}>
+        <label htmlFor="stree">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formInputValidity.street && <p>Please enter a street</p>}
+      </div>
+      <div className={postalControlStyles}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalInputRef} />
+        {!formInputValidity.postalCode && (
+          <p>Please enter a postal code (5 digits) </p>
+        )}
+      </div>
+      <div className={cityControlStyles}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formInputValidity.city && <p>Please enter a valid city</p>}
+      </div>
+      <div className={styles.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={styles.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
